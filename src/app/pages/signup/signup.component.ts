f3cc6cfeb4516662b7bdf8558847dc816a448721
aryamansharma <49034013+aryamansharma@users.noreply.github.com>
2023-09-30 23:50:02 +0530

    db entry of user or signing up user

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+// services
+import { AuthService } from 'src/app/services/auth.service';
+
+// angular form
+import { NgForm } from '@angular/forms';
+import { finalize } from 'rxjs/operators';
+
+// Firebase
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+
+// Browser image resizer
+import { readAndCompressImage } from 'browser-image-resizer';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  picture: string =
+    'https://learnyst.s3.amazonaws.com/assets/schools/2410/resources/images/logo_lco_i3oab.png';
 
+  uploadPercent: any = null;
+
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private db: AngularFireDatabase,
+    private storage: AngularFireStorage,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {}
+
+  onSubmit(f: NgForm) {
+    const { email, password, username, country, bio, name } = f.form.value;
+
+    this.auth
+      .signUp(email, password)
+      .then((res) => {
+        console.log(res);
+        const uid = res?.user?.uid;
+
+        this.db.object(`/users/${uid}`).set({
+          id: uid,
+          name: name,
+          email: email,
+          instaUserName: username,
+          country: country,
+          bio: bio,
+          picture: this.picture,
+        });
+      })
+      .then(() => {
+        this.router.navigateByUrl('/');
+        this.toastr.success('SignUp Successfull');
+      })
+      .catch((err) => {
+        console.log(err);
+        this.toastr.error('SignUp Failed');
+      });
+  }
 }
